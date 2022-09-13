93c957933bccca058518869a0fcd2b84ea31224a
kimbugweronnie <kimbugweronnie@gmail.com>
2022-09-13 15:36:24 +0300

    adding calculator

---
+++ calculator.js
@@ -0,0 +1,16 @@
+function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
+    
+}
+function getAnswer(result){
+     eval(result);
+    return typeof(result)
+}
+function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
+    
+}
+
+function clr(){
+    form.displayResult.value= null;
+ }
